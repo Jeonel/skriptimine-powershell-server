3a5bb4a7dad5df95b3dc90b159adfeafe89535c0
unknown <administrator@SV.KOOL>
2023-12-04 12:39:14 +0200

    tegin kustutamise skripti aduseri jaoks

---
+++ ylesanne4.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module ActiveDirectory
+
+$Eesnimi = Read-Host "Sisesta kasutaja eesnimi"
+$Perenimi = Read-Host "Sisesta kasutaja perekonnanimi"
+
+$Kasutajanimi = "$Eesnimi.$Perenimi"
+
+try {
+    Remove-ADUser -Identity $Kasutajanimi -Confirm:$false -ErrorAction Stop
+    Write-Host "User $Kasutajanimi is removed succesfully."
+}
+catch {
+    Write-Host "User not exists or problem is occuring during user removing. $($_.Exception.Message)"
+}
